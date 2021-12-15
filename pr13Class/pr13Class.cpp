2b62a86d323a7f3afbb8a889047c0c72de9e6fa6
NikolaiKharitonov <77926593+NikolaiKharitonov@users.noreply.github.com>
2021-12-15 19:02:48 +0300

    pr13

--- pr13Class/pr13Class.cpp
+++ pr13Class/pr13Class.cpp
@@ -7,6 +7,21 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+    cout << "Hello World!\n";
+    char a[1024];
+    char b[1024];
+    cin >> a;
+    cin >> b;
+
+    StringLine ST(a);
+    StringLine ST2(b);
+    cout << "Копирование части строки " << ST.Copy(2, 3) << "\n";
+    cout << "Удаление части строки " << ST.Delete(2, 3) << "\n";
+    cout << "Длина строки " << ST.GetLenght() << "\n";
+    cout << "" << ST << "\n";
+    ST = ST + ST2;
+    cout << "Соединение строк " << ST << "\n";
 
 }
 
